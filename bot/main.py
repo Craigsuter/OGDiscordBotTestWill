16e8d8d985631b05d0f8449d3c8b308b17a8f3df
Will (Craigsuter2) <willcrane78@gmail.com>
2022-06-15 22:02:55 +0000

    added a checker for the cs event to not create for the faze game or games without confirmed teams

--- bot/main.py
+++ bot/main.py
@@ -4819,8 +4819,8 @@ async def testingspam():
         lines= "empty"
 
       try:
-      
-        if lines[0] == linetocheck:
+        counter=teams.count('/')
+        if lines[0] == linetocheck or teams == "FaZe vs OG" or counter>0:
           pass
         else:
           eventdata = await guild.create_scheduled_event(name=name, description=description, start_time=time, end_time=end_time, entity_type=discord.enums.EntityType(3), location=gamepage)
